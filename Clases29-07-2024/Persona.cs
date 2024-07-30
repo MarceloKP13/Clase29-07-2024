0dcc4e91651f0ed464cdbc7b5a67bf7ceffbed62
Marcelo Torres <kevin.torres85@outlook.es>
2024-07-29 19:54:08 -0500

    clase 29-07-2024

---
+++ Clases29-07-2024/Persona.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases29_07_2024
+{
+    public class Persona
+    { //llave de inicio de la clase
+        /*Creando un atributo
+         * La sintaxis para crear un atributo es:
+         * 1. modificador de acceso: Define si el atributo es accesible o no fuera de la clase
+         * 2. tipo de dato
+         * 3. nombre del atributo
+         * 4. colocar ';'
+         */
+
+        public string nombre;
+        public int edad;
+        public int edad2 = 15;
+        public char genero = 'M';
+
+        /* Métodos
+        Son las acciones principales que realiza el objeto
+        Sintaxis:
+        1. Modificador de acceso
+        2. colocar void: No recibe ni devuleve ningun parametro
+
+
+        Método constructor
+        El constructo es un método especial que se utiliza para inicializar los atributos de la clase
+        Sintaxis:
+
+        1. 
+         */
+
+        public Persona(string nombre, int edad, char genero)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.genero = genero;
+        }
+
+        public void mostrarInfo()
+        {
+            Console.WriteLine($"Hola {nombre}, tienes {edad} y tu género es {genero}");
+            if (edad >= 18)
+            {
+                Console.WriteLine($"Tienes {edad} y eres MAYOR de edad");
+            }
+            else
+            {
+                Console.WriteLine($"Tienes {edad} y eres MENOR de edad");
+            }
+
+        } //llave de cierre de la clase
+
+        public void mayorMenorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine("Es MAYOR de edad->metodo2");
+            }
+            else
+            {
+                Console.WriteLine("Es MENOR de edad->metodo2");
+            }
+        }
+    }
+}
---
+++ Clases29-07-2024/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases29_07_2024
+{
+    internal class Program
+    {
+        /*Clases y objetos
+        Es una plantilla o molde que se utiliza para representar propiedades y métodos
+        Propiedades--> Son las caracteristicas principales de un objeto, las represento a 
+        través de variables.
+        
+        Métodos-> Son las acciones principales que realiza el objeto para representar estas acciones
+        utilizo los métodos
+
+        Atributos-> 
+        ¿Qué es declarar e inicializar una variable?
+        Declarar-> Definir el tipo y el nombre
+        Inicializar->cuando se le asigna un valor a la variable
+         */
+
+
+        static void Main(string[] args)
+        {
+            int n1 = 8;
+            Console.WriteLine(n1);
+
+            /*Pasos para crear un objeto
+            1. Instanciar la clase : significa crear un objeto
+            2. Colocar el nombre de la variable de tipo "Persona"
+            3. Utilizar el operador de la asignación
+            */
+
+            Persona p = new Persona("Marce,",22,'M');
+
+            //A través del operador '.' (punto) se puede visualizar los atributos y métodos que sean que tengan
+            //el modificador public
+
+            /*p.nombre = "Marcelo";*/
+            Console.WriteLine(p.nombre);
+            Console.Write("Escribe tu edad: ");
+
+            //Desde la clase Persona
+            p.edad = int.Parse(Console.ReadLine());
+            p.mostrarInfo();
+
+            p.mayorMenorEdad ();
+
+            //desde la clase principal
+            Console.WriteLine($"La edad 2° es {p.edad2}");
+            if (p.edad2 >= 18)
+            {
+                Console.WriteLine("Es MAYOR de edad");
+            }else
+            {
+                Console.WriteLine("Es MENOR de edad");
+            }
+
+            Persona p1 = new Persona("Marce,", 22, 'M');
+            /*p1.edad = 25;
+            Console.WriteLine($"La edad es {p1.edad} ");*/
+
+            p1.mayorMenorEdad();
+
+            Console.ReadKey();
+
+            //Exponer tecnicas de estudio que yo utilizo. Con presentación.
+        }
+
+    }
+}
